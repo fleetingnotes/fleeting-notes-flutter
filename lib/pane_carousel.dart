60a057f33ad9cb392211f53bd541019921fdd0cd
Matthew Wong <matthewwong@Matthews-MacBook-Air.local>
2022-02-07 23:13:00 -0500

    convert panecarousel to stateless widget

--- lib/pane_carousel.dart
+++ lib/pane_carousel.dart
@@ -3,7 +3,7 @@ import 'package:carousel_slider/carousel_slider.dart';
 
 import 'pane.dart';
 
-class PaneCarousel extends StatefulWidget {
+class PaneCarousel extends StatelessWidget {
   const PaneCarousel(
       {Key? key,
       required this.paneQueries,
@@ -18,19 +18,14 @@ class PaneCarousel extends StatefulWidget {
   final CarouselController carouselController;
 
   @override
-  State<PaneCarousel> createState() => _PaneCarouselState();
-}
-
-class _PaneCarouselState extends State<PaneCarousel> {
-  @override
   Widget build(BuildContext context) {
     return CarouselSlider(
         options: CarouselOptions(
           enableInfiniteScroll: false,
-          onPageChanged: widget.onPageChanged,
+          onPageChanged: onPageChanged,
         ),
-        carouselController: widget.carouselController,
-        items: widget.paneQueries
+        carouselController: carouselController,
+        items: paneQueries
             .map(
               (item) => Builder(builder: (BuildContext context) {
                 return Pane(query: item);
