d4f33571d4b3754a1da86994be7957c50b9babb7
unknown <matthewwong525@gmail.com>
2022-02-20 13:47:17 -0500

    override textfield controller for stylable text in textfield

---
+++ lib/models/text_part_style_definitions.dart
@@ -0,0 +1,50 @@
+import 'package:fleeting_notes_flutter/models/text_part_style_definition.dart';
+
+class TextPartStyleDefinitions {
+  TextPartStyleDefinitions({required this.definitionList});
+
+  final List<TextPartStyleDefinition> definitionList;
+
+  RegExp createCombinedPatternBasedOnStyleMap() {
+    final String combinedPatternString = definitionList
+        .map<String>(
+          (TextPartStyleDefinition textPartStyleDefinition) =>
+              textPartStyleDefinition.pattern,
+        )
+        .join('|');
+
+    return RegExp(
+      combinedPatternString,
+      multiLine: true,
+      caseSensitive: false,
+    );
+  }
+
+  TextPartStyleDefinition? getStyleOfTextPart(
+    String textPart,
+    String text,
+  ) {
+    return List<TextPartStyleDefinition?>.from(definitionList).firstWhere(
+      (TextPartStyleDefinition? styleDefinition) {
+        if (styleDefinition == null) return false;
+
+        bool hasMatch = false;
+
+        RegExp(styleDefinition.pattern, caseSensitive: false)
+            .allMatches(text)
+            .forEach(
+          (RegExpMatch currentMatch) {
+            if (hasMatch) return;
+
+            if (currentMatch.group(0) == textPart) {
+              hasMatch = true;
+            }
+          },
+        );
+
+        return hasMatch;
+      },
+      orElse: () => null,
+    );
+  }
+}
