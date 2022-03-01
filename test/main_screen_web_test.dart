a15f59436bec0b3c079ee61a2b16d02e79abe8e2
Matthew Wong <matthewwong@Matthews-MacBook-Air.local>
2022-03-01 12:21:50 -0500

    fix realmdb mock and add new test for new note button press

---
+++ test/main_screen_web_test.dart
@@ -0,0 +1,44 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility that Flutter provides. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:fleeting_notes_flutter/screens/main/main_screen.dart';
+import 'package:fleeting_notes_flutter/models/Note.dart';
+import 'package:fleeting_notes_flutter/screens/note/note_screen.dart';
+import 'mock_realm_db.dart';
+
+// Currently Only Testing Web
+void main() {
+  setUpAll(() {
+    registerFallbackValue(Note.empty());
+  });
+  testWidgets('Render Main Screen', (WidgetTester tester) async {
+    tester.binding.window.physicalSizeTestValue = const Size(3000, 1500);
+    MockRealmDB mockDb = MockRealmDB();
+    // when(() => mockDb.getSearchNotes(any()))
+    //     .thenAnswer((_) async => Future.value([]));
+    // when(() => mockDb.getBacklinkNotes(any()))
+    //     .thenAnswer((_) async => Future.value([]));
+    await tester.pumpWidget(MaterialApp(home: MyHomePage(db: mockDb)));
+    expect(find.bySemanticsLabel('Title'), findsOneWidget);
+    expect(find.bySemanticsLabel('Note'), findsOneWidget);
+    expect(find.byIcon(Icons.add), findsOneWidget);
+    expect(find.byIcon(Icons.search), findsOneWidget);
+    expect(find.byIcon(Icons.menu), findsOneWidget);
+  });
+  testWidgets('Press new note button adds new note',
+      (WidgetTester tester) async {
+    tester.binding.window.physicalSizeTestValue = const Size(3000, 1000);
+    MockRealmDB mockDb = MockRealmDB();
+    await tester.pumpWidget(MaterialApp(home: MyHomePage(db: mockDb)));
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pump();
+    expect(find.byType(NoteScreen), findsNWidgets(2));
+  });
+}
