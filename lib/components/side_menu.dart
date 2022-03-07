e3a685c057c99f12bda6a8f8f69392e4d707fa89
Matthew Wong <matthewwong@Matthews-MacBook-Air.local>
2022-03-07 10:30:42 -0500

    rename to settings_screen from setting_screen

--- lib/components/side_menu.dart
+++ lib/components/side_menu.dart
@@ -1,4 +1,4 @@
-import 'package:fleeting_notes_flutter/screens/settings/setting_screen.dart';
+import 'package:fleeting_notes_flutter/screens/settings/settings_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:fleeting_notes_flutter/constants.dart';
