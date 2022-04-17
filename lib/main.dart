0c11e3f890940a65ea4810d1001c8450f9bc48c6
Kevin Liu <kevin.f.liu2012@gmail.com>
2022-04-16 22:13:19 -0500

    add firebase lib (from matt's pr) and upgrade packages

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,18 @@ import 'package:fleeting_notes_flutter/models/Note.dart';
 import 'package:fleeting_notes_flutter/my_app.dart'
     if (dart.library.js) 'package:fleeting_notes_flutter/my_app_web.dart';
 
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   Hive.initFlutter();
   Hive.registerAdapter(NoteAdapter());
   await Hive.openBox('settings');
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
