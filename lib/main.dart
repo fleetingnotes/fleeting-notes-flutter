cdc5e11941acf6ceee29b0637719f3cf57697633
Matthew Wong <matthewwong525@gmail.com>
2022-04-17 10:13:00 -0400

    add openHiveBox function to properly open box in mobile (#196)

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:fleeting_notes_flutter/models/Note.dart';
+import 'package:flutter/foundation.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:fleeting_notes_flutter/my_app.dart'
     if (dart.library.js) 'package:fleeting_notes_flutter/my_app_web.dart';
 
+Future<Box> openHiveBox(String boxName) async {
+  if (!kIsWeb && !Hive.isBoxOpen(boxName)) {
+    Hive.init((await getApplicationDocumentsDirectory()).path);
+  }
+  return await Hive.openBox(boxName);
+}
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  Hive.initFlutter();
+  await Hive.initFlutter();
   Hive.registerAdapter(NoteAdapter());
-  await Hive.openBox('settings');
+  await openHiveBox('settings');
   runApp(const MyApp());
 }
