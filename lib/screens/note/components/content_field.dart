73cdb22d21984458ff665b8b53faa9af9fe27fa8
Matthew Wong <matthewwong525@gmail.com>
2022-05-07 21:53:56 -0400

    Link preview (#246)
    * rename title_links and follow_link
    
    * convert follow_link button to link_preview
    
    * update tests

--- lib/screens/note/components/content_field.dart
+++ lib/screens/note/components/content_field.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:fleeting_notes_flutter/screens/note/components/title_links.dart';
+import 'package:fleeting_notes_flutter/screens/note/components/link_suggestions.dart';
 import 'package:fleeting_notes_flutter/models/Note.dart';
-import 'package:fleeting_notes_flutter/screens/note/components/follow_link.dart';
+import 'package:fleeting_notes_flutter/screens/note/components/link_preview.dart';
 import 'package:fleeting_notes_flutter/database.dart';
 import 'package:flutter/services.dart';
 
@@ -161,7 +161,7 @@ class _ContentFieldState extends State<ContentField> {
         child: ValueListenableBuilder(
             valueListenable: titleLinkQuery,
             builder: (context, value, child) {
-              return TitleLinks(
+              return LinkSuggestions(
                 caretOffset: caretOffset,
                 allLinks: allLinks,
                 query: titleLinkQuery.value,
@@ -176,9 +176,13 @@ class _ContentFieldState extends State<ContentField> {
   }
 
   void showFollowLinkOverlay(context, String title, BoxConstraints size) async {
-    void _onFollowLinkTap() async {
+    Future<Note> getFollowLinkNote() async {
       Note? note = await widget.db.getNoteByTitle(title);
       note ??= Note.empty(title: title);
+      return note;
+    }
+
+    void _onFollowLinkTap(Note note) async {
       widget.db.navigateToNote(note); // TODO: Deprecate
       await widget.db.firebase.analytics.logEvent(name: 'follow_link');
       removeOverlay();
@@ -191,12 +195,21 @@ class _ContentFieldState extends State<ContentField> {
       Theme.of(context).textTheme.bodyText2!,
       size,
     );
-    FollowLink builder(context) {
-      return FollowLink(
-        caretOffset: caretOffset,
-        onTap: _onFollowLinkTap,
-        layerLink: layerLink,
-      );
+    Widget builder(context) {
+      return FutureBuilder<Note>(
+          future: getFollowLinkNote(),
+          builder: (BuildContext context, AsyncSnapshot<Note> snapshot) {
+            if (snapshot.hasData && snapshot.data != null) {
+              return LinkPreview(
+                note: snapshot.data!,
+                caretOffset: caretOffset,
+                onTap: () => _onFollowLinkTap(snapshot.data!),
+                layerLink: layerLink,
+              );
+            } else {
+              return const Center(child: CircularProgressIndicator());
+            }
+          });
     }
 
     overlayContent(builder);
---
+++ lib/screens/note/components/link_preview.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import '../../../models/Note.dart';
+
+class LinkPreview extends StatelessWidget {
+  const LinkPreview({
+    Key? key,
+    required this.note,
+    required this.caretOffset,
+    required this.onTap,
+    required this.layerLink,
+  }) : super(key: key);
+
+  final Note note;
+  final Offset caretOffset;
+  final VoidCallback onTap;
+  final LayerLink layerLink;
+
+  @override
+  Widget build(BuildContext context) {
+    double width = 300;
+    Offset newCaretOffset = caretOffset;
+    if (width + caretOffset.dx > layerLink.leaderSize!.width) {
+      newCaretOffset =
+          Offset(layerLink.leaderSize!.width - width, caretOffset.dy);
+    }
+    return Positioned(
+      width: width,
+      height: 150,
+      child: CompositedTransformFollower(
+        link: layerLink,
+        offset: newCaretOffset,
+        child: Material(
+          child: OutlinedButton(
+              onPressed: onTap,
+              style: const ButtonStyle(alignment: Alignment.topLeft),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 15),
+                  Text(
+                    note.title,
+                    maxLines: 1,
+                    textAlign: TextAlign.start,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                      color: Theme.of(context).textTheme.bodyText1?.color,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    note.content,
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 3,
+                    style: Theme.of(context).textTheme.bodyText2,
+                  ),
+                  const Spacer(),
+                  Text(note.getDateTimeStr(),
+                      style: Theme.of(context).textTheme.caption),
+                  const SizedBox(height: 15)
+                ],
+              )),
+        ),
+      ),
+    );
+  }
+}
