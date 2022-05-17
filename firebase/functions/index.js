574e7a370a847d8aa3a4832de8a65fd150effcb9
Matthew Wong <matthewwong525@gmail.com>
2022-05-17 08:16:06 -0400

    Firebase get all notes (#255)
    * auth working for cloud function
    
    * add function to retrieve all notes
    
    * fix lint
    
    * remove comments

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,7 +1,15 @@
-require('@tensorflow/tfjs-node');
-const functions = require("firebase-functions");
-const use = require("@tensorflow-models/universal-sentence-encoder");
+// CORS Express middleware to enable CORS Requests.
+const cors = require('cors')({origin: true});
+const request = require('request');
 
+// Firebase Setup
+const functions = require("firebase-functions");
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+// tensorflow setup
+require('@tensorflow/tfjs-node');
+const use = require("@tensorflow-models/universal-sentence-encoder");
 let model;
 
 // Calculate the dot product of two vector arrays.
@@ -22,7 +30,6 @@ const zipWith = (f, xs, ys) => {
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
-
 exports.rank_sentence_similarity = functions.runWith({memory: "1GB"}).https.onRequest(async (req, res) => {
     // Embed an array of sentences.
     const sentences = [
@@ -41,3 +48,108 @@ exports.rank_sentence_similarity = functions.runWith({memory: "1GB"}).https.onRe
         res.send(sentenceMap);
     });
 });
+
+
+
+exports.get_all_notes = functions.https.onRequest(async (req, res) => {
+    const handleError = (email, error) => {
+      functions.logger.error({ User: email }, error);
+      res.sendStatus(500);
+      return;
+    };
+  
+    const handleResponse = (email, status, body) => {
+      functions.logger.log(
+        { User: email },
+        {
+          Response: {
+            Status: status,
+          },
+        }
+      );
+      if (body) {
+        return res.status(200).json(body);
+      }
+      return res.sendStatus(status);
+    };
+    let email = '';
+    try {
+      return cors(req, res, async () => {
+        // Authentication requests are POSTed, other requests are forbidden
+        if (req.method !== 'POST') {
+          return handleResponse(email, 403);
+        }
+        email = req.body.email;
+        if (!email) {
+          return handleResponse(email, 400);
+        }
+        const password = req.body.password;
+        if (!password) {
+          return handleResponse(email, 400);
+        }
+  
+        const authRes = await authenticate(email, password)
+        const valid = authRes['error'] === undefined;
+        if (!valid) {
+          return handleResponse(email, 401); // Invalid username/password
+        }
+        var db = admin.firestore();
+        var notes = []
+        const uid = authRes['localId'];
+        const snapshot = await db.collection("notes").where('_partition', '==', uid).get()
+
+
+        snapshot.forEach(doc => {
+          var newNote = {
+            "_id": doc.id,
+            "title": doc.data().title,
+            "content": doc.data().content,
+            "source": doc.data().source,
+            "timestamp": doc.data().created_timestamp.toDate(),
+            "_isDeleted": doc.data()._isDeleted,
+          }
+          notes = notes.concat(newNote);
+        });
+        return handleResponse(email, 200, notes);
+      });
+    } catch (error) {
+      return handleError(email, error);
+    }
+});
+
+/**
+ * Authenticate the provided credentials.
+ * @returns {Promise<JSON>} success or failure.
+ */
+ function authenticate(email, password) {
+    // For the purpose of this example use httpbin (https://httpbin.org) and send a basic authentication request.
+    // (Only a password of `Testing123` will succeed)
+    const firebaseApiKey= 'AIzaSyBXON2_4OVoHSplISHIQKtexvAlZEIZBRY'
+    const authEndpoint = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${firebaseApiKey}`;
+    const creds = {
+      "email": email,
+      "password": password,
+    };
+    return new Promise((resolve, reject) => {
+      request.post({
+        url: authEndpoint,
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: creds,
+        json: true,
+      }, (error, response, body) => {
+        if (error) {
+          return reject(error);
+        }
+        const statusCode = response ? response.statusCode : 0;
+        if (statusCode === 401 || statusCode === 400) { // Invalid username/password
+          return resolve(body);
+        }
+        if (statusCode !== 200) {
+          return reject(new Error(`invalid response returned from ${authEndpoint} status code ${statusCode}`));
+        }
+        return resolve(body);
+      });
+    });
+  }
