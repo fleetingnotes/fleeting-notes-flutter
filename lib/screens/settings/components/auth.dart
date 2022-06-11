ab7ed8ce8b619b2a754c39e53956ab14121d03fd
Matthew Wong <matthewwong525@gmail.com>
2022-06-11 19:02:46 -0400

    Auth UI (#280)
    * use flutterfire ui for authentication
    
    * add deps for other platforms
    
    * remove flutterfire-ui and make settings screen scrollable
    
    * reset .lock files to main

--- lib/screens/settings/components/auth.dart
+++ lib/screens/settings/components/auth.dart
@@ -1,7 +1,10 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 import '../../../database.dart';
 import 'login_dialog.dart';
+
+enum AuthAction { signIn, signUp }
 
 class Auth extends StatefulWidget {
   const Auth({Key? key, required this.db, this.onLogin}) : super(key: key);
@@ -13,19 +16,10 @@ class Auth extends StatefulWidget {
 }
 
 class _AuthState extends State<Auth> {
-  String email = '';
-  String password = '';
   bool isLoading = false;
+  AuthAction _authAction = AuthAction.signIn;
 
-  bool validEmail() {
-    return email.isNotEmpty;
-  }
-
-  bool validPassword() {
-    return password.isNotEmpty;
-  }
-
-  void onDialogContinue() async {
+  void onDialogContinue(String email, String password) async {
     Navigator.pop(context);
     await widget.db.firebase.logoutAllSessions();
     widget.db.login(email, password);
@@ -38,18 +32,15 @@ class _AuthState extends State<Auth> {
     widget.db.firebase.analytics.logEvent(name: 'login_dialog_see_pricing');
   }
 
-  Future<void> onLoginPress() async {
-    setState(() {
-      isLoading = true;
-    });
-    bool isLoggedIn = await _login();
+  Future<void> onLoginPress(String email, String password) async {
+    bool isLoggedIn = await _login(email, password);
     if (isLoggedIn && !await widget.db.firebase.isCurrUserPremium()) {
       await showDialog(
         context: context,
         barrierDismissible: false,
         builder: (BuildContext context) {
           return LoginDialog(
-            onContinue: onDialogContinue,
+            onContinue: () => onDialogContinue(email, password),
             onSeePricing: onSeePricing,
           );
         },
@@ -63,34 +54,34 @@ class _AuthState extends State<Auth> {
         duration: Duration(seconds: 2),
       ));
     }
-    setState(() {
-      isLoading = false;
-    });
   }
 
-  Future<bool> _login() async {
-    if (!validPassword() || !validEmail()) {
-      return false;
-    }
+  Future<bool> _login(String email, String password) async {
     bool isLoggedIn = await widget.db.login(email, password);
     return isLoggedIn;
   }
 
-  Future<void> _register() async {
-    if (!validPassword() || !validEmail()) {
-      return;
-    }
-    setState(() {
-      isLoading = true;
-    });
+  Future<void> _register(String email, String password) async {
     bool isRegistered = await widget.db.register(email, password);
     if (isRegistered) {
-      _login();
+      bool isLoggedIn = await _login(email, password);
+      if (isLoggedIn && widget.onLogin != null) widget.onLogin!(email);
     } else {
       ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
         content: Text('Registration failed'),
         duration: Duration(seconds: 2),
       ));
+    }
+  }
+
+  void onSubmit(String email, String password) async {
+    setState(() {
+      isLoading = true;
+    });
+    if (_authAction == AuthAction.signIn) {
+      await onLoginPress(email, password);
+    } else if (_authAction == AuthAction.signUp) {
+      await _register(email, password);
     }
     setState(() {
       isLoading = false;
@@ -99,51 +90,187 @@ class _AuthState extends State<Auth> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
+    return (isLoading)
+        ? const Padding(
+            padding: EdgeInsets.all(20),
+            child: Center(child: CircularProgressIndicator()),
+          )
+        : Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              (_authAction == AuthAction.signIn)
+                  ? AuthFlow(
+                      text: "Don't have an account? ",
+                      linkedText: "Register",
+                      onTap: () {
+                        setState(() {
+                          _authAction = AuthAction.signUp;
+                        });
+                      },
+                    )
+                  : AuthFlow(
+                      text: "Already have an account? ",
+                      linkedText: "Sign in",
+                      onTap: () {
+                        setState(() {
+                          _authAction = AuthAction.signIn;
+                        });
+                      },
+                    ),
+              EmailForm(
+                action: _authAction,
+                onSubmit: onSubmit,
+              ),
+            ],
+          );
+  }
+}
+
+class AuthFlow extends StatelessWidget {
+  const AuthFlow({
+    Key? key,
+    required this.text,
+    required this.linkedText,
+    required this.onTap,
+  }) : super(key: key);
+
+  final String text;
+  final String linkedText;
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(0, 15, 0, 15),
+      child: RichText(
+        text: TextSpan(children: [
+          TextSpan(text: text),
+          TextSpan(
+            text: linkedText,
+            style: const TextStyle(color: Colors.blue),
+            recognizer: TapGestureRecognizer()..onTap = onTap,
+          ),
+        ]),
+      ),
+    );
+  }
+}
+
+class EmailForm extends StatefulWidget {
+  const EmailForm({
+    Key? key,
+    required this.action,
+    required this.onSubmit,
+  }) : super(key: key);
+
+  final AuthAction action;
+  final Function onSubmit;
+
+  @override
+  State<EmailForm> createState() => _EmailFormState();
+}
+
+class _EmailFormState extends State<EmailForm> {
+  String email = '';
+  String password = '';
+  String confirmPassword = '';
+  final _formKey = GlobalKey<FormState>();
+
+  String? validateEmailField() {
+    if (email.isEmpty) {
+      return 'Email is required';
+    }
+    if (!RegExp(
+            r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+        .hasMatch(email)) {
+      return 'Invalid email';
+    }
+    return null;
+  }
+
+  String? validatePasswordField() {
+    if (password.isEmpty) {
+      return 'Password is required';
+    }
+    if (password.length < 6) {
+      return 'Password must be at least 6 characters';
+    }
+    return null;
+  }
+
+  String? validateConfirmPasswordField() {
+    if (confirmPassword.isEmpty) {
+      return 'Confirm password is required';
+    }
+    if (confirmPassword != password) {
+      return 'Passwords do not match';
+    }
+    return null;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          TextField(
+          TextFormField(
             decoration: const InputDecoration(
-              hintText: 'Email',
+              labelText: 'Enter your email',
+              border: OutlineInputBorder(),
             ),
-            keyboardType: TextInputType.emailAddress,
-            enableSuggestions: false,
-            autocorrect: false,
-            onChanged: (String val) {
+            onChanged: (String value) {
               setState(() {
-                email = val;
+                email = value;
               });
             },
+            validator: (_) => validateEmailField(),
           ),
-          TextField(
+          const SizedBox(height: 10),
+          TextFormField(
             decoration: const InputDecoration(
-              hintText: 'Password',
+              labelText: 'Password',
+              border: OutlineInputBorder(),
             ),
             obscureText: true,
-            enableSuggestions: false,
-            autocorrect: false,
-            onChanged: (String val) {
+            onChanged: (String value) {
               setState(() {
-                password = val;
+                password = value;
               });
             },
+            validator: (_) => validatePasswordField(),
           ),
-          Container(
-            margin: const EdgeInsets.only(top: 20),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                    child: const Text('Login'),
-                    onPressed: (isLoading) ? null : onLoginPress),
-                ElevatedButton(
-                  child: const Text('Register'),
-                  onPressed: (isLoading) ? null : _register,
-                ),
-              ],
-            ),
-          ),
+          (widget.action == AuthAction.signUp)
+              ? Column(
+                  children: [
+                    const SizedBox(height: 10),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        labelText: 'Confirm password',
+                        border: OutlineInputBorder(),
+                      ),
+                      obscureText: true,
+                      onChanged: (String value) {
+                        setState(() {
+                          confirmPassword = value;
+                        });
+                      },
+                      validator: (_) => validateConfirmPasswordField(),
+                    ),
+                  ],
+                )
+              : const SizedBox(height: 0),
+          const SizedBox(height: 20),
+          OutlinedButton(
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  widget.onSubmit(email, password);
+                }
+              },
+              child: (widget.action == AuthAction.signUp)
+                  ? const Text('Register', style: TextStyle(fontSize: 15))
+                  : const Text('Sign in', style: TextStyle(fontSize: 15))),
         ],
       ),
     );
