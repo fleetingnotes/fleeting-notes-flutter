27826c21f7964aa0d57cbcae66503bc8f83ee22f
Matthew Wong <matthewwong525@gmail.com>
2022-06-11 20:07:38 -0400

    Bq export firebase func (#281)
    * refactor firebase functions & add hourly user bigquery export
    
    * install bigquery properly
    
    * update bq_user_export

--- firebase/functions/bq_user_export.js
+++ firebase/functions/bq_user_export.js
@@ -1,21 +1,22 @@
 const functions = require("firebase-functions");
 
 const admin = require('firebase-admin');
-const { BigQuery } = require('@google-cloud/bigquery');
+const { BigQuery, BigQueryDatetime } = require('@google-cloud/bigquery');
 
 const bigquery = new BigQuery();
 
 const listAllUsers = async () => {
     const appendToAllUsers = async (allUsers, nextPageToken) => {
+        const export_time = new BigQueryDatetime(new Date().toISOString())
         // List batch of users, 1000 at a time.
         let listUsersResult = await admin.auth().listUsers(1000, nextPageToken)
         listUsersResult.users.forEach((userRecord) => {
             userRecord.metadata.creationTime = new Date(userRecord.metadata.creationTime)
             allUsers.push({
                 uid: userRecord.uid,
-                last_sign_in_time: new Date(userRecord.metadata.lastSignInTime).toISOString(),
-                creation_time: new Date(userRecord.metadata.creationTime).toISOString(),
-                export_time: new Date().toISOString(),
+                last_sign_in_time: new BigQueryDatetime(new Date(userRecord.metadata.lastSignInTime).toISOString()),
+                creation_time: new BigQueryDatetime(new Date(userRecord.metadata.creationTime).toISOString()),
+                export_time: export_time,
             })
         });
         return listUsersResult.pageToken;
@@ -34,5 +35,7 @@ exports.bq_user_export = functions.pubsub.schedule('every hour').onRun(async () 
     const dataset = bigquery.dataset('firebase_functions');
     const table = dataset.table('auth_users');
 
-    return await table.insert(allUsers);
+    await table.insert(allUsers);
+
+    return null;
 });
