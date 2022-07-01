3248770152488f0215a40096f7c37ca6dd6df508
Matthew Wong <matthewwong525@gmail.com>
2022-07-01 08:36:24 -0400

    add update notes function to fleeting notes

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,14 +1,15 @@
 const { initializeApp } = require('firebase-admin/app');
 initializeApp();
 
-const get_all_notes = require('./get_all_notes');
+const firestore = require('./firestore');
 const logout_all_sessions = require('./logout_all_sessions');
 const bq_user_export = require('./bq_user_export');
 const rank_sentence_similarity = require('./rank_sentence_similarity');
 const bq_notes_export = require('./bq_notes_export');
 // const scripts = require('./scripts');
 
-exports.get_all_notes = get_all_notes.get_all_notes;
+exports.get_all_notes = firestore.get_all_notes;
+exports.update_notes = firestore.update_notes;
 exports.logout_all_sessions = logout_all_sessions.logout_all_sessions;
 exports.new_logout_all_sessions = logout_all_sessions.new_logout_all_sessions;
 exports.rank_sentence_similarity = rank_sentence_similarity.rank_sentence_similarity;
