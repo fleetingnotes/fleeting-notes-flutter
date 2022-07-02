9cd424a800671ca8d0073211b7fef3d3c42538e4
Matthew Wong <matthewwong525@gmail.com>
2022-07-02 13:19:51 -0400

    update shared notes on refresh

--- lib/database.dart
+++ lib/database.dart
@@ -44,8 +44,8 @@ class Database {
   Future<List<Note>> getAllNotes({forceSync = false}) async {
     try {
       var box = await Hive.openBox(firebase.userId);
-      if (((box.isEmpty || forceSync) &&
-          (isLoggedIn() || firebase.isSharedNotes))) {
+      if ((((box.isEmpty || forceSync) && isLoggedIn()) ||
+          firebase.isSharedNotes)) {
         List<Note> notes =
             await firebase.getAllNotes(isShared: firebase.isSharedNotes);
         Map<String, Note> noteIdMap = {for (var note in notes) note.id: note};
