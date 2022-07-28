ee5d4a554003eda07a6a12e169dd49fccc3dd187
Matthew Wong <matthewwong525@gmail.com>
2022-07-28 10:01:55 -0400

    Increase max lines & handle back button android (#301)
    * set max lines to 20
    
    * handle back button better to go back to search screen

--- lib/screens/note/components/content_field.dart
+++ lib/screens/note/components/content_field.dart
@@ -261,7 +261,7 @@ class _ContentFieldState extends State<ContentField> {
           autofocus: widget.autofocus,
           controller: widget.controller,
           minLines: 5,
-          maxLines: 10,
+          maxLines: 20,
           style: Theme.of(context).textTheme.bodyText2,
           decoration: const InputDecoration(
             hintText: "Note and links to other ideas",
