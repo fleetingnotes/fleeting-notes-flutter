e8896d2feeffa4d3139ea5ca7fe353b1efcd3980
Matthew Wong <matthewwong525@gmail.com>
2022-11-07 19:47:01 -0500

    In app purchase (#416)
    * add in app purchases & storekit
    
    * add in app purchases for ios
    
    * remove commented lines. add apple iap
    
    * remove storekit configuration

--- lib/screens/settings/components/login_dialog.dart
+++ lib/screens/settings/components/login_dialog.dart
@@ -1,18 +1,68 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
+import 'dart:io' show Platform;
+import 'package:in_app_purchase/in_app_purchase.dart';
 
 class LoginDialog extends StatelessWidget {
   const LoginDialog({
     Key? key,
     required this.onContinue,
     required this.onSeePricing,
+    required this.userId,
   }) : super(key: key);
 
   final VoidCallback onContinue;
   final VoidCallback onSeePricing;
+  final String userId;
+
+  void _listenToPurchaseUpdated(List<PurchaseDetails> purchaseDetailsList) {
+    // ignore: avoid_function_literals_in_foreach_calls
+    purchaseDetailsList.forEach((PurchaseDetails purchaseDetails) async {
+      if (purchaseDetails.status == PurchaseStatus.pending) {
+      } else {
+        if (purchaseDetails.status == PurchaseStatus.error) {
+        } else if (purchaseDetails.status == PurchaseStatus.purchased ||
+            purchaseDetails.status == PurchaseStatus.restored) {}
+        if (purchaseDetails.pendingCompletePurchase) {
+          await InAppPurchase.instance.completePurchase(purchaseDetails);
+        }
+      }
+    });
+  }
+
+  void oniOSSubscribe() async {
+    var _kIds = {'fn_basic_plan'};
+    var _iap = InAppPurchase.instance;
+    final res = await _iap.queryProductDetails(_kIds);
+    if (res.notFoundIDs.isEmpty) {
+      var purchaseParam = PurchaseParam(
+        productDetails: res.productDetails.first,
+        applicationUserName: userId,
+      );
+      _iap.buyNonConsumable(purchaseParam: purchaseParam);
+
+      // listen for subscription to be complete
+      var _subscription = _iap.purchaseStream.listen((purchaseDetailsList) {
+        _listenToPurchaseUpdated(purchaseDetailsList);
+      });
+      _subscription.onDone(_subscription.cancel);
+      _subscription.onError((_) => _subscription.cancel());
+    }
+  }
 
   List<Widget> getActionButtons() {
-    if (!kIsWeb) {
+    if (!kIsWeb && Platform.isIOS) {
+      return [
+        TextButton(
+          child: const Text('Continue'),
+          onPressed: onContinue,
+        ),
+        ElevatedButton(
+          child: const Text('Subscribe'),
+          onPressed: oniOSSubscribe,
+        ),
+      ];
+    } else if (!kIsWeb) {
       return [
         ElevatedButton(
           child: const Text('Continue'),
