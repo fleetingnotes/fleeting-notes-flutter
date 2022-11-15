7955d5248868a443ace4ac1ab39119d991b47727
Matthew Wong <matthewwong525@gmail.com>
2022-11-15 09:44:14 -0500

    enhance android home screen widget

--- android/app/src/main/kotlin/com/fleetingnotes/ListWidgetService.kt
+++ android/app/src/main/kotlin/com/fleetingnotes/ListWidgetService.kt
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.net.Uri
+import android.view.View
 import android.widget.RemoteViews;
 import android.widget.RemoteViewsService;
 import kotlinx.serialization.*
@@ -40,8 +41,9 @@ class ListRemoteViewsFactory(
   override fun getViewAt(position: Int): RemoteViews {
       // Construct a remote views item based on the widget item XML file,
       // and set the text based on the position.
-      return RemoteViews(context.packageName, R.layout.item_view).apply {
-          setTextViewText(R.id.contentTextView, noteItems[position].content)
+      return RemoteViews(context.packageName, R.layout.note_card_layout).apply {
+          setTextViewText(R.id.title_text_view, noteItems[position].title)
+          setTextViewText(R.id.content_text_view, noteItems[position].content)
           // Next, set a fill-intent, which will be used to fill in the pending intent template
           // that is set on the collection view in StackWidgetProvider.
           val fillInIntent = Intent().apply {
@@ -52,7 +54,7 @@ class ListRemoteViewsFactory(
           }
           // Make it possible to distinguish the individual on-click
           // action of a given item
-          setOnClickFillInIntent(R.id.noteContainer, fillInIntent)
+          setOnClickFillInIntent(R.id.note_container, fillInIntent)
       }
   }
   override fun onDataSetChanged() {
