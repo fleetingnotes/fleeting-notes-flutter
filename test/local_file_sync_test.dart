b2eaf5ef7ac77c3d5a3a78eb1b4286aa49ac49a7
Matthew Wong <matthewwong525@gmail.com>
2022-12-21 10:40:59 -0500

    add test for renaming files & update init tests

--- test/local_file_sync_test.dart
+++ test/local_file_sync_test.dart
@@ -11,7 +11,6 @@ import 'package:flutter_test/flutter_test.dart';
 import 'mocks/mock_database.dart';
 import 'mocks/mock_local_file_sync.dart';
 import 'mocks/mock_settings.dart';
-// import 'package:path/path.dart' as p;
 import 'mocks/mock_supabase.dart';
 import 'utils.dart';
 
@@ -61,6 +60,27 @@ void main() {
         expect(lfs.fs.directory(lfs.syncDir).listSync().isEmpty, isTrue);
       },
     );
+    testWidgets(
+      "Local changes update renamed file",
+      (WidgetTester tester) async {
+        var settings = MockSettings();
+        var lfs = await setupLfs(settings: settings);
+        await fnPumpWidget(tester, const MaterialApp(home: MainScreen()),
+            settings: settings, localFs: lfs);
+        await addNote(tester, title: "hello-world", content: "");
+
+        expect(find.byType(NoteCard), findsOneWidget);
+        File file = lfs.fs.directory(lfs.syncDir).listSync().first as File;
+        var newPath =
+            file.path.replaceFirst('hello-world.md', 'hello-world-mod.md');
+        renameFile(file, lfs, newPath);
+        await modifyCurrentNote(tester, content: 'a modification');
+        expect(
+          lfs.fs.file(newPath).readAsStringSync().contains("a modification"),
+          isTrue,
+        );
+      },
+    );
   });
   group("Changing filesystem notes", () {
     testWidgets(
@@ -93,7 +113,7 @@ void main() {
 
         expect(find.byType(NoteCard), findsOneWidget);
         File file = lfs.fs.directory(lfs.syncDir).listSync().first as File;
-        modifyFile(file, lfs, "a modification");
+        appendFile(file, lfs, "a modification");
         await tester.pumpAndSettle();
         expect(
             find.descendant(
@@ -118,30 +138,60 @@ void main() {
         expect(find.byType(NoteCard), findsNothing);
       },
     );
+    testWidgets(
+      "Renaming from filesystem notes still updates",
+      (WidgetTester tester) async {
+        var settings = MockSettings();
+        var lfs = await setupLfs(settings: settings);
+        await fnPumpWidget(tester, const MaterialApp(home: MainScreen()),
+            settings: settings, localFs: lfs);
+        await addNote(tester, title: "hello-world", content: "");
+
+        expect(find.byType(NoteCard), findsOneWidget);
+        File file = lfs.fs.directory(lfs.syncDir).listSync().first as File;
+        var newPath =
+            file.path.replaceFirst('hello-world.md', 'hello-world-mod.md');
+        renameFile(file, lfs, newPath);
+        appendFile(lfs.fs.file(newPath), lfs, "a modification");
+        await tester.pumpAndSettle();
+        expect(
+            find.descendant(
+                of: find.bySemanticsLabel('Note and links to other ideas'),
+                matching: find.text('a modification', findRichText: true)),
+            findsOneWidget);
+      },
+    );
   });
   group("Init local file sync", () {
+    Future<File> setupInitLFS(
+        WidgetTester tester, DateTime fileLastModified) async {
+      // setup mocks
+      var settings = MockSettings();
+      var lfs = await setupLfs(settings: settings);
+      var supabase = getBaseMockSupabaseDB();
+      var db = MockDatabase(
+          supabase: supabase, settings: settings, localFileSync: lfs);
+
+      // setup notes
+      var n = Note.empty(title: "hello-world", content: "local mod");
+      var file = lfs.fs.file(p.join(lfs.syncDir, "${n.title}.md"));
+      file.writeAsStringSync(
+          n.getMarkdownContent().replaceFirst("local mod", "filesystem mod"));
+      file.setLastModifiedSync(fileLastModified);
+      await tester.pump(const Duration(seconds: 1));
+      await db.upsertNotes([n]);
+      await lfs.init(notes: await db.getAllNotes()); // run sync
+
+      await fnPumpWidget(tester, const MaterialApp(home: MainScreen()),
+          settings: settings, localFs: lfs, db: db);
+      return file;
+    }
+
     testWidgets(
       "Prioritizes local if modified last",
       (WidgetTester tester) async {
-        // setup mocks
-        var settings = MockSettings();
-        var lfs = await setupLfs(settings: settings);
-        var supabase = getBaseMockSupabaseDB();
-        var db = MockDatabase(
-            supabase: supabase, settings: settings, localFileSync: lfs);
-
-        // setup notes
-        var n = Note.empty(title: "hello-world", content: "local mod");
-        var file = lfs.fs.file(p.join(lfs.syncDir, "${n.title}.md"));
-        file.writeAsStringSync(
-            n.getMarkdownContent().replaceFirst("local mod", "filesystem mod"));
-        await tester.pump(const Duration(seconds: 1));
-        await db.upsertNotes([n]);
-        await lfs.init(notes: await db.getAllNotes()); // run sync
-
-        await fnPumpWidget(tester, const MaterialApp(home: MainScreen()),
-            settings: settings, localFs: lfs, db: db);
-
+        var fileLastModified = DateTime.now().subtract(const Duration(days: 1));
+        var file = await setupInitLFS(tester, fileLastModified);
         await tester.tap(find.byType(NoteCard));
         await tester.pumpAndSettle();
         expect(
@@ -156,24 +206,8 @@ void main() {
     testWidgets(
       "Prioritizes filesystem if modified last",
       (WidgetTester tester) async {
-        // setup mocks
-        var settings = MockSettings();
-        var lfs = await setupLfs(settings: settings);
-        var supabase = getBaseMockSupabaseDB();
-        var db = MockDatabase(
-            supabase: supabase, settings: settings, localFileSync: lfs);
-
-        // setup notes
-        var n = Note.empty(title: "hello-world", content: "local mod");
-        File file = lfs.fs.file(p.join(lfs.syncDir, "${n.title}.md"));
-        await db.upsertNotes([n]);
-        await tester.pump(const Duration(seconds: 1));
-        file.writeAsStringSync(
-            n.getMarkdownContent().replaceFirst("local mod", "filesystem mod"));
-        await lfs.init(notes: await db.getAllNotes()); // run sync
-
-        await fnPumpWidget(tester, const MaterialApp(home: MainScreen()),
-            settings: settings, localFs: lfs, db: db);
+        var fileLastModified = DateTime.now().add(const Duration(days: 1));
+        var file = await setupInitLFS(tester, fileLastModified);
 
         await tester.tap(find.byType(NoteCard));
         await tester.pumpAndSettle();
@@ -197,7 +231,7 @@ void deleteFile(File file, MockLocalFileSync lfs) {
   lfs.dirController.add(e);
 }
 
-void modifyFile(File file, MockLocalFileSync lfs, String appendText) {
+void appendFile(File file, MockLocalFileSync lfs, String appendText) {
   file.writeAsStringSync(appendText, mode: FileMode.append);
   var e = MockFileSystemEvent();
   when(() => e.path).thenReturn(file.path);
@@ -213,4 +247,15 @@ void addFile(File file, MockLocalFileSync lfs, String fileContents) {
   lfs.dirController.add(e);
 }
 
+void renameFile(File file, MockLocalFileSync lfs, String destination) {
+  file.renameSync(destination);
+  var e = MockFileSystemMoveEvent();
+  when(() => e.path).thenReturn(file.path);
+  when(() => e.type).thenReturn(FileSystemEvent.move);
+  when(() => e.destination).thenReturn(destination);
+  lfs.dirController.add(e);
+}
+
 class MockFileSystemEvent extends Mock implements FileSystemEvent {}
+
+class MockFileSystemMoveEvent extends Mock implements FileSystemMoveEvent {}
