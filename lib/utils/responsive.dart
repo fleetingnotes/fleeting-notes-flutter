f6dec8c2332f15a9502be4895b9c810a6c62e647
Matthew Wong <matthewwong525@gmail.com>
2023-01-19 12:17:20 -0500

    make responsive class work better with UI

--- lib/utils/responsive.dart
+++ lib/utils/responsive.dart
@@ -16,17 +16,17 @@ class Responsive extends StatelessWidget {
       MediaQuery.of(context).size.width < 599;
 
   static bool isTablet(BuildContext context) =>
-      MediaQuery.of(context).size.width < 839 &&
+      MediaQuery.of(context).size.width < 999 &&
       MediaQuery.of(context).size.width >= 600;
 
   static bool isDesktop(BuildContext context) =>
-      MediaQuery.of(context).size.width >= 840;
+      MediaQuery.of(context).size.width >= 1000;
 
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
       builder: (context, constraints) {
-        if (constraints.maxWidth >= 840) {
+        if (constraints.maxWidth >= 1000) {
           return desktop;
         } else if (constraints.maxWidth >= 600) {
           return tablet;
