be89f71dbd44eaccdc2c4e080742bfc0b5c3087d
Matthew Wong <matthewwong525@gmail.com>
2023-02-01 18:11:07 -0500

    remove dep on dart:html

--- lib/services/browser_ext/default.dart
+++ lib/services/browser_ext/default.dart
@@ -1,12 +1,10 @@
 import 'dart:async';
-// ignore: avoid_web_libraries_in_flutter
-import 'dart:html';
 import 'package:flutter/foundation.dart';
 
 class BE {
   StreamController<Uint8List?> pasteController =
       StreamController<Uint8List?>.broadcast();
-  StreamController blurController = StreamController<Event>.broadcast();
+  StreamController blurController = StreamController.broadcast();
   Future<String> getSelectionText() async => '';
   Future<String> getSourceUrl({String defaultText = ''}) async => defaultText;
 }
