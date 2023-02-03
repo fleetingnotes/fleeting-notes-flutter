8b021dd4c737dd4267899d6f2291264a8b8ba73a
Matthew Wong <matthewwong525@gmail.com>
2023-02-03 16:18:43 -0500

    add xcode container for web-ext-safari

---
+++ web-ext-safari-app/iOS (App)/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  iOS (App)
+//
+//  Created by Matthew Wong on 2023-02-03.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+}
---
+++ web-ext-safari-app/macOS (App)/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  macOS (App)
+//
+//  Created by Matthew Wong on 2023-02-03.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
