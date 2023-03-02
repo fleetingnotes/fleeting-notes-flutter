abbafa4ca82863264d12b7c195361dd5a0a218ed
Matthew Wong <matthewwong525@gmail.com>
2023-03-02 15:13:06 -0500

    make expanded work as expected

--- lib/widgets/note_card.dart
+++ lib/widgets/note_card.dart
@@ -106,6 +106,7 @@ class _NoteCardState extends State<NoteCard> {
                                 maxLines: widget.maxLines,
                               ),
                             ),
+                          if (widget.expanded) const Spacer(),
                         ],
                       ),
                     ),
