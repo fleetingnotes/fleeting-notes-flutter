56b87691547fd21ab806cbaf7f5e8403f9944906
Matthew Wong <matthewwong525@gmail.com>
2023-04-17 11:55:59 -0400

    Bump version to 0.8.7 (#581)
    * Bump version to 0.8.7
    
    * only allow android & ios to initHomeWidget
    
    * add com.fleetingnotes to keychain-access-groups
    
    * sign dmg

--- windows/build.dart
+++ windows/build.dart
@@ -10,7 +10,7 @@ void main() {
   InnoSetup(
     app: InnoSetupApp(
       name: 'Fleeting Notes',
-      version: Version.parse('0.8.6'),
+      version: Version.parse('0.8.7'),
       publisher: 'Matthew Wong',
       urls: InnoSetupAppUrls(
         homeUrl: Uri.parse('https://fleetingnotes.app/'),
