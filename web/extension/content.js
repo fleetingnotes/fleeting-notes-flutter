48fc3fe128ed0012b5ce523ffa7aac1bb382935a
Matthew Wong <matthewwong525@gmail.com>
2023-04-22 16:55:52 -0400

    Browser ext local clipping (#593)
    * allow source metadata to be populated into URL to create note
    
    * add frontend function to gather source info & lint
    
    * remove console.log

--- web/extension/content.js
+++ web/extension/content.js
@@ -1,11 +1,11 @@
 // Parse the current page URL for the video ID and returns the ID.
 // https://stackoverflow.com/questions/3452546/how-do-i-get-the-youtube-video-id-from-a-url
 function getVideoId() {
-  if (window.location.search.split('v=')[1] === undefined) {
+  if (window.location.search.split("v=")[1] === undefined) {
     return "";
   }
-  let videoId = window.location.search.split('v=')[1];
-  let ampersandPosition = videoId.indexOf('&');
+  let videoId = window.location.search.split("v=")[1];
+  let ampersandPosition = videoId.indexOf("&");
   // filter out irrevelant query part
   if (ampersandPosition != -1) {
     videoId = videoId.substring(0, ampersandPosition);
@@ -19,19 +19,20 @@ function getCurrentTimestampInfo() {
   const videoId = getVideoId();
   // obtain title by scraping webpage
   const title = document.getElementsByClassName(
-      "title style-scope ytd-video-primary-info-renderer"
+    "title style-scope ytd-video-primary-info-renderer",
   )[0].firstChild.innerText;
 
   return {
     timestamp: Math.floor(time),
     videoId,
     title,
-  }
+  };
 }
 
 // sidebar logic
-let sidebarWidth = parseInt(window.localStorage.getItem('sidebarWidth')) || 365;
-let sidebarHeight = parseInt(window.localStorage.getItem('sidebarHeight')) || 400;
+let sidebarWidth = parseInt(window.localStorage.getItem("sidebarWidth")) || 365;
+let sidebarHeight = parseInt(window.localStorage.getItem("sidebarHeight")) ||
+  400;
 let rootElement;
 let sidebar;
 let closeBtn;
@@ -45,7 +46,7 @@ const createSidebar = () => {
   const className = "sidebar";
   const rootShadow = rootElement.attachShadow({ mode: "closed" });
   const url = `${chrome.runtime.getURL("web-ext.html")}`;
-  
+
   rootShadow.innerHTML = `
     <style>
       .${className} {
@@ -96,15 +97,15 @@ const createSidebar = () => {
   sidebar?.style.setProperty("right", `${-sidebarWidth}px`);
 
   // sidebar listener
-  closeBtn = rootShadow.querySelector('#close-fab');
-  closeBtn.addEventListener('click', (e) => {
-    if (e.target.id === 'close-fab') {
+  closeBtn = rootShadow.querySelector("#close-fab");
+  closeBtn.addEventListener("click", (e) => {
+    if (e.target.id === "close-fab") {
       toggleSidebar();
     }
   });
 
-  resizer = rootShadow.querySelector('#resizer');
-  resizer.addEventListener('mousedown', initDrag, false);
+  resizer = rootShadow.querySelector("#resizer");
+  resizer.addEventListener("mousedown", initDrag, false);
 
   return rootElement;
 };
@@ -116,7 +117,7 @@ function updateSidebarDimensions({ width = null, height = null }) {
   }
   if (height) {
     sidebar?.style.setProperty("height", `${height}px`);
-    closeBtn?.style.setProperty("top", `${height + 15}px`)
+    closeBtn?.style.setProperty("top", `${height + 15}px`);
     sidebarHeight = height;
   }
 }
@@ -129,10 +130,10 @@ function initDrag(e) {
   // expand resizer so iframe doesnt eat up inputs
   resizer.style.top = 0;
   resizer.style.right = 0;
-  resizer.style.height = '100%';
-  resizer.style.width = '100%';
-  document.documentElement.addEventListener('mousemove', doDrag, false);
-  document.documentElement.addEventListener('mouseup', stopDrag, false);
+  resizer.style.height = "100%";
+  resizer.style.width = "100%";
+  document.documentElement.addEventListener("mousemove", doDrag, false);
+  document.documentElement.addEventListener("mouseup", stopDrag, false);
 }
 
 function doDrag(e) {
@@ -142,59 +143,59 @@ function doDrag(e) {
   console.log(width, height);
   updateSidebarDimensions({
     width: (width > 250) ? width : 250,
-    height: (height > 300) ? height: 300,
-  })
+    height: (height > 300) ? height : 300,
+  });
 }
 
 function stopDrag(e) {
-  window.localStorage.setItem('sidebarWidth', sidebarWidth);
-  window.localStorage.setItem('sidebarHeight', sidebarHeight);
+  window.localStorage.setItem("sidebarWidth", sidebarWidth);
+  window.localStorage.setItem("sidebarHeight", sidebarHeight);
   // shrink resizer
   resizer?.style.setProperty("right", `${sidebarWidth}px`);
   resizer?.style.setProperty("top", `${sidebarHeight}px`);
   resizer?.style.setProperty("width", "10px");
   resizer?.style.setProperty("height", "10px");
-  document.documentElement.removeEventListener('mousemove', doDrag, false);
-  document.documentElement.removeEventListener('mouseup', stopDrag, false);
+  document.documentElement.removeEventListener("mousemove", doDrag, false);
+  document.documentElement.removeEventListener("mouseup", stopDrag, false);
 }
 
-
 function modifySrc(src) {
-    const ytRegex = /https:\/\/www.youtube.com\/watch\?v=.+/;
-    if (src.match(ytRegex)) {
-      const timestampData = getCurrentTimestampInfo();
-      src = `https://www.youtube.com/watch?v=${timestampData.videoId}&t=${timestampData.timestamp}s`
-    }
-    return src;
+  const ytRegex = /https:\/\/www.youtube.com\/watch\?v=.+/;
+  if (src.match(ytRegex)) {
+    const timestampData = getCurrentTimestampInfo();
+    src =
+      `https://www.youtube.com/watch?v=${timestampData.videoId}&t=${timestampData.timestamp}s`;
+  }
+  return src;
 }
 
 const getIframeSrc = () => {
   const selectionText = window.getSelection().toString().trim();
   const src = modifySrc(window.location.href);
   let url = `${chrome.runtime.getURL("web-ext.html")}?`;
-  url += (selectionText) ? `content=${encodeURIComponent(selectionText)}&` : '';
+  url += (selectionText) ? `content=${encodeURIComponent(selectionText)}&` : "";
   url += `source=${encodeURIComponent(src)}&`;
   return url;
-}
+};
 
 const toggleSidebar = (src) => {
-  if (sidebar?.style.right.startsWith('-')) {
+  if (sidebar?.style.right.startsWith("-")) {
     document.body.appendChild(rootElement);
     if (!src) {
       src = getIframeSrc();
     }
-    sidebar.src = src
+    sidebar.src = src;
     sidebar?.style.setProperty("right", "8px");
-    closeBtn?.style.setProperty("right", "8px")
-    resizer?.style.setProperty("display", "block")
+    closeBtn?.style.setProperty("right", "8px");
+    resizer?.style.setProperty("display", "block");
     sidebar.contentWindow.focus();
   } else if (src) {
     sidebar.src = src;
     sidebar.contentWindow.focus();
   } else {
     sidebar?.style.setProperty("right", `-${sidebarWidth}px`);
-    closeBtn?.style.setProperty("right", `-${sidebarWidth}px`)
-    resizer?.style.setProperty("display", "none")
+    closeBtn?.style.setProperty("right", `-${sidebarWidth}px`);
+    resizer?.style.setProperty("display", "none");
     setTimeout(() => document.body.removeChild(rootElement), 500);
   }
 };
@@ -203,10 +204,38 @@ function initSidebar() {
   rootElement = createSidebar();
 }
 
+function getSrcInfo() {
+  const sourceTitle = document.title ||
+    document.querySelector("meta[property=og\\:title]")?.content;
+  const sourceDescription = document.querySelector('meta[name="description"]')
+    ?.content ||
+    document.querySelector("meta[property=og\\:description]")?.content;
+  let sourceImageUrl = document.querySelector("meta[property=og\\:image]")
+    ?.content;
+
+  if (!sourceImageUrl) {
+    const iconHref = document.querySelector("link[rel='shortcut icon']")
+      ?.getAttribute(
+        "href",
+      );
+    sourceImageUrl = (iconHref?.startsWith("/"))
+      ? window.location.origin + iconHref
+      : iconHref;
+  }
+
+  const srcInfo = {
+    "source": modifySrc(window.location.href),
+    "source_title": sourceTitle,
+    "source_description": sourceDescription,
+    "source_image_url": sourceImageUrl,
+  };
+  return srcInfo;
+}
+
 // listeneres
 function listenMessages() {
   // Listen for add timestamp request msg from popup
-  chrome.runtime.onMessage.addListener((request, sender, response) => {
+  chrome.runtime.onMessage.addListener((request, _sender, response) => {
     if (request.msg === "get-selection-text") {
       const selectionText = window.getSelection().toString();
       response(selectionText);
@@ -214,8 +243,8 @@ function listenMessages() {
       toggleSidebar(request.src);
       response(true);
     } else if (request.msg === "get-src") {
-      const src = modifySrc(window.location.href);
-      response(src);
+      const srcInfo = getSrcInfo();
+      response(srcInfo);
     }
     return true; // used to send response async
   });
@@ -223,3 +252,4 @@ function listenMessages() {
 
 initSidebar();
 listenMessages();
+
