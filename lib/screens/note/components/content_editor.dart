f790f348a23398a41645e925921d9fe6ea2441c2
Matthew Wong <matthewwong525@gmail.com>
2023-04-28 09:49:49 -0400

    add placeholder text when super editor is empty

--- lib/screens/note/components/content_editor.dart
+++ lib/screens/note/components/content_editor.dart
@@ -49,16 +49,101 @@ class _EditorState extends ConsumerState<ContentEditor> {
   Widget build(BuildContext context) {
     final editorData = ref.read(editorProvider);
     return SuperEditor(
-      editor: DocumentEditor(document: editorData.contentDoc),
-      composer: DocumentComposer(),
-      autofocus: widget.autofocus,
-      // stylesheet: Stylesheet(
-      //   rules: [],
-      //   inlineTextStyler: (a, t) {
-      //     return t;
-      //   },
-      //   documentPadding: EdgeInsets.zero,
-      // ),
+        editor: DocumentEditor(document: editorData.contentDoc),
+        autofocus: widget.autofocus,
+        stylesheet: Stylesheet(
+          rules: defaultStylesheet.rules,
+          inlineTextStyler: defaultStylesheet.inlineTextStyler,
+          documentPadding: EdgeInsets.zero,
+        ),
+        componentBuilders: [
+          const EmptyHintComponentBuilder(),
+          ...defaultComponentBuilders,
+        ]);
+  }
+}
+
+TextStyle _textStyleBuilder(Set<Attribution> attributions) {
+  // We only care about altering a few styles. Start by getting
+  // the standard styles for these attributions.
+  var newStyle = defaultStyleBuilder(attributions);
+
+  // Style headers
+  for (final attribution in attributions) {
+    if (attribution == emptyAttribution) {
+      newStyle = newStyle.copyWith(
+        color: const Color(0xFF444444),
+        fontSize: 14,
+      );
+    }
+  }
+
+  return newStyle;
+}
+
+class EmptyHintComponentBuilder implements ComponentBuilder {
+  const EmptyHintComponentBuilder();
+
+  @override
+  SingleColumnLayoutComponentViewModel? createViewModel(
+      Document document, DocumentNode node) {
+    if (node is! ParagraphNode) {
+      return null;
+    }
+    // We only care about the situation where the Document is empty. In this case
+    // a Document is "empty" when there is only a single ParagraphNode.
+    if (document.nodes.length > 1) {
+      return null;
+    }
+    // We only care about the situation where the first ParagraphNode is empty.
+    if (node.text.text.isNotEmpty) {
+      return null;
+    }
+    // This component builder can work with the standard paragraph view model.
+    // We'll defer to the standard paragraph component builder to create it.
+    return ParagraphComponentViewModel(
+        nodeId: node.id,
+        text: node.text,
+        blockType: emptyAttribution,
+        textStyleBuilder: _textStyleBuilder,
+        selectionColor: defaultSelectionStyle.selectionColor);
+  }
+
+  @override
+  Widget? createComponent(SingleColumnDocumentComponentContext componentContext,
+      SingleColumnLayoutComponentViewModel componentViewModel) {
+    if (componentViewModel is! ParagraphComponentViewModel) {
+      return null;
+    }
+
+    final blockAttribution = componentViewModel.blockType;
+    if (blockAttribution != emptyAttribution) {
+      return null;
+    }
+
+    final textSelection = componentViewModel.selection;
+    return TextWithHintComponent(
+      key: componentContext.componentKey,
+      text: componentViewModel.text,
+      textStyleBuilder: _textStyleBuilder,
+      metadata: componentViewModel.blockType != null
+          ? {
+              'blockType': componentViewModel.blockType,
+            }
+          : {},
+      // This is the text displayed as a hint.
+      hintText: AttributedText(
+        text: 'Start writing your thoughts...',
+      ),
+      // This is the function that selects styles for the hint text.
+      hintStyleBuilder: (Set<Attribution> attributions) =>
+          _textStyleBuilder(attributions).copyWith(
+        color: const Color(0xFFDDDDDD),
+      ),
+      textSelection: textSelection,
+      selectionColor: componentViewModel.selectionColor,
     );
   }
 }
+
+const emptyAttribution = NamedAttribution('empty');
