7f16c1c408c5821fc37f3fc8ac1bf69fa8255f0c
Matthew Wong <matthewwong525@gmail.com>
2023-06-03 17:00:00 -0400

    Bump version to 0.10.1 (#637)
    * Bump version to 0.10.1
    
    * copy to clipboard when sharing note from web

--- lib/screens/note/components/note_editor_app_bar.dart
+++ lib/screens/note/components/note_editor_app_bar.dart
@@ -1,5 +1,6 @@
-import 'dart:typed_data';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:share_plus/share_plus.dart';
 
@@ -27,11 +28,28 @@ class NoteEditorAppBar extends ConsumerWidget {
   final TextEditingController? contentController;
   final TextEditingController? titleController;
 
+  void _onShare(BuildContext context) async {
+    if (kIsWeb) {
+      await Clipboard.setData(ClipboardData(text: contentController?.text));
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+        behavior: SnackBarBehavior.floating,
+        content: Text("Copied Note Content"),
+        showCloseIcon: true,
+      ));
+      return;
+    }
+    final box = context.findRenderObject() as RenderBox?;
+    Share.share(
+      contentController?.text ?? '',
+      subject: titleController?.text,
+      sharePositionOrigin: box!.localToGlobal(Offset.zero) & box.size,
+    );
+  }
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final noteUtils = ref.watch(noteUtilsProvider);
     final n = note;
-    final box = context.findRenderObject() as RenderBox?;
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
       child: Row(
@@ -39,20 +57,14 @@ class NoteEditorAppBar extends ConsumerWidget {
           IconButton(onPressed: onClose, icon: const Icon(Icons.close)),
           const Spacer(),
           NotePopupMenu(
-              note: note,
-              onAddAttachment: (String fn, Uint8List? fb) {
-                if (n == null) return;
-                noteUtils.onAddAttachment(context, n, fn, fb,
-                    controller: contentController);
-              },
-              onShare: () {
-                Share.share(
-                  contentController?.text ?? '',
-                  subject: titleController?.text,
-                  sharePositionOrigin:
-                      box!.localToGlobal(Offset.zero) & box.size,
-                );
-              }),
+            note: note,
+            onAddAttachment: (String fn, Uint8List? fb) {
+              if (n == null) return;
+              noteUtils.onAddAttachment(context, n, fn, fb,
+                  controller: contentController);
+            },
+            onShare: () => _onShare(context),
+          ),
           if (onBacklinks != null)
             OutlinedButton(
               onPressed: onBacklinks,
