834e0916e06023ac2309b4f7014bc9cc7cae5417
Matthew Wong <matthewwong525@gmail.com>
2023-06-08 09:02:46 -0400

    make source field unfocusable on traversal

--- lib/screens/note/components/SourceField/source_container.dart
+++ lib/screens/note/components/SourceField/source_container.dart
@@ -105,12 +105,10 @@ class _SourceContainerState extends ConsumerState<SourceContainer> {
   Widget build(BuildContext context) {
     final m = widget.metadata;
     if (m != null) {
-      return ExcludeFocusTraversal(
-        child: SourcePreview(
-          metadata: m,
-          onPressed: () => onPressedPreview(m.url),
-          onClear: widget.onClearSource,
-        ),
+      return SourcePreview(
+        metadata: m,
+        onPressed: () => onPressedPreview(m.url),
+        onClear: widget.onClearSource,
       );
     }
     return TextField(
