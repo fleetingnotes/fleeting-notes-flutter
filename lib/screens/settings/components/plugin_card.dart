95b4982810b1befdc8e38e4632532638d0a7a591
Gonzalo Velasco <vela8624@gmail.com>
2023-07-24 12:46:39 -0300

    Plugin selector (#720)
    * add plugin card
    
    * add plugin selector dialog
    
    * use the new plugin selector in settings
    
    * remove unnecesary dependencies
    
    * fix performance error
    
    * keep the old button

---
+++ lib/screens/settings/components/plugin_card.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+
+class Plugin {
+  final String key;
+  final String title;
+  final String description;
+  final String commandId;
+
+  Plugin(
+      {required this.key,
+      required this.title,
+      required this.description,
+      required this.commandId});
+}
+
+class PluginCard extends StatefulWidget {
+  const PluginCard({
+    Key? key,
+    required this.plugin,
+    this.onSelect,
+    this.onTap,
+    this.isActive = false,
+    this.isSelected = false,
+    this.expanded = false,
+    this.maxLines,
+  }) : super(key: key);
+
+  final bool isActive;
+  final bool isSelected;
+  final bool expanded;
+  final VoidCallback? onSelect;
+  final VoidCallback? onTap;
+  final Plugin plugin;
+  final int? maxLines;
+
+  @override
+  State<PluginCard> createState() => _PluginCardState();
+}
+
+class _PluginCardState extends State<PluginCard> {
+  bool hovering = false;
+
+  onSelect(bool? value) {
+    if (value == true) {
+      widget.onSelect?.call();
+    }
+    if (value == false && widget.isSelected) {
+      widget.onTap?.call();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double elevation = (widget.isSelected) ? 1 : 0;
+    return GestureDetector(
+      onLongPress: widget.onSelect,
+      onTap: widget.onTap,
+      child: MouseRegion(
+        onEnter: (_) => setState(() {
+          hovering = true;
+        }),
+        onExit: (_) => setState(() {
+          hovering = false;
+        }),
+        child: Card(
+          clipBehavior: Clip.hardEdge,
+          elevation: elevation,
+          shadowColor: Colors.transparent,
+          shape: RoundedRectangleBorder(
+            side: BorderSide(
+              color: Theme.of(context).colorScheme.outlineVariant,
+            ),
+            borderRadius: const BorderRadius.all(Radius.circular(12)),
+          ),
+          child: Stack(
+            children: [
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Flexible(
+                    child: Padding(
+                      padding: const EdgeInsets.all(16),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          if (widget.plugin.title.isNotEmpty)
+                            Padding(
+                              padding: const EdgeInsets.only(bottom: 8.0),
+                              child: Text(
+                                widget.plugin.title,
+                                style: Theme.of(context).textTheme.titleMedium,
+                                maxLines: widget.maxLines,
+                              ),
+                            ),
+                          if (widget.plugin.description.isNotEmpty)
+                            Flexible(
+                              fit: (widget.expanded)
+                                  ? FlexFit.tight
+                                  : FlexFit.loose,
+                              child: Text(
+                                widget.plugin.description,
+                                style: Theme.of(context).textTheme.bodySmall,
+                                maxLines: widget.maxLines,
+                              ),
+                            ),
+                          if (widget.expanded &&
+                              widget.plugin.description.isEmpty)
+                            const Spacer(),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/settings/components/plugin_commands_setting.dart
+++ lib/screens/settings/components/plugin_commands_setting.dart
@@ -1,3 +1,4 @@
+import 'package:fleeting_notes_flutter/screens/settings/components/plugin_card.dart';
 import 'package:fleeting_notes_flutter/screens/settings/components/plugin_command.dart';
 import 'package:fleeting_notes_flutter/screens/settings/components/settings_title.dart';
 import 'package:fleeting_notes_flutter/services/providers.dart';
@@ -6,13 +7,58 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:collection/collection.dart';
+import 'package:fleeting_notes_flutter/screens/settings/components/plugin_selector_dialog.dart';
+
+final List<Plugin> plugins = [
+  Plugin(
+    key: 'plugin1',
+    title: 'ChatGPT',
+    description:
+        'This plugin passes the content of your note into ChatGPT and also gives you the option to adjust a pre-defined prompt in the metadata.',
+    commandId: 'official/chat-gpt-3.5',
+  ),
+  Plugin(
+    key: 'plugin2',
+    title: 'Clip Web to Markdown',
+    description:
+        'This plugin fetches the source of your note and convert it into markdown.',
+    commandId: 'official/clip-web-to-md',
+  ),
+  Plugin(
+    key: 'plugin3',
+    title: 'Current timestamp',
+    description: 'This plugin insert the current timestamp in a note.',
+    commandId: 'official/current-timestamp',
+  ),
+  Plugin(
+    key: 'plugin4',
+    title: 'OCR source',
+    description:
+        'This plugin utilizes Google Vision to fetch and convert the source of your image in text.',
+    commandId: 'official/ocr-source',
+  ),
+  Plugin(
+    key: 'option3',
+    title: 'Speech To Text',
+    description:
+        'This plugin utilizes Whisper from ChatGPT to fetch and convert the source of your speech.',
+    commandId: 'official/speech-to-text',
+  ),
+  Plugin(
+    key: 'plugin5',
+    title: 'Summarize Source',
+    description:
+        'This plugin fetches the source of your note and summarizes it into key points using ChatGPT',
+    commandId: 'official/summarize-source',
+  ),
+];
 
 class PluginCommandSetting extends ConsumerWidget {
   const PluginCommandSetting({
     super.key,
   });
 
-  void upsertSlashCommand(
+  static void upsertSlashCommand(
     Settings settings, {
     int? index,
     Map<String, String>? commandSettings = const {
@@ -67,7 +113,23 @@ class PluginCommandSetting extends ConsumerWidget {
                     iconColor: colorScheme.onPrimaryContainer,
                     title: const Text('Add New Command'),
                     leading: const Icon(Icons.add),
-                    onTap: () => upsertSlashCommand(settings),
+                    onTap: () => showDialog(
+                      context: context,
+                      builder: (c) => PluginSelectorDialog(
+                        plugins: plugins,
+                        onPluginSelected: (Plugin selectedPlugin) {
+                          upsertSlashCommand(
+                            settings,
+                            index: null,
+                            commandSettings: {
+                              "alias": selectedPlugin.title,
+                              "commandId": selectedPlugin.commandId,
+                              "metadata": "",
+                            },
+                          );
+                        },
+                      ),
+                    ),
                   ),
                 ],
               );
