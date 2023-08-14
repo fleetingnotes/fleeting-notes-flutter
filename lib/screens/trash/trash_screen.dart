d7a722218344207d47a07b19db39a3b8b9603a8f
Gonzalo Velasco <vela8624@gmail.com>
2023-08-14 15:31:31 -0300

    remove MaterialBannerExample

--- lib/screens/trash/trash_screen.dart
+++ lib/screens/trash/trash_screen.dart
@@ -74,32 +74,3 @@ class _TrashScreenState extends ConsumerState<TrashScreen> {
     );
   }
 }
-
-class MaterialBannerExample extends StatelessWidget {
-  const MaterialBannerExample({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('The MaterialBanner is below'),
-      ),
-      body: const MaterialBanner(
-        padding: EdgeInsets.all(20),
-        content: Text('Hello, I am a Material Banner'),
-        leading: Icon(Icons.agriculture_outlined),
-        backgroundColor: Color(0xFFE0E0E0),
-        actions: <Widget>[
-          TextButton(
-            onPressed: null,
-            child: Text('OPEN'),
-          ),
-          TextButton(
-            onPressed: null,
-            child: Text('DISMISS'),
-          ),
-        ],
-      ),
-    );
-  }
-}
