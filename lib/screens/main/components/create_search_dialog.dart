72aed1a426d7b497944b10e10f13281292833054
Gonzalo Velasco <vela8624@gmail.com>
2023-08-16 15:50:28 -0300

    use dynamic dialog and implement edit

--- lib/screens/main/components/create_search_dialog.dart
+++ lib/screens/main/components/create_search_dialog.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:fleeting_notes_flutter/widgets/dialog_page.dart';
 
 class CreateSearchDialog extends StatefulWidget {
   final Function(String) addSearch;
   final Function(int) removeSearch;
+  final Function(int, String) editSearch;
   final List<String> searches;
 
   const CreateSearchDialog({
@@ -10,6 +12,7 @@ class CreateSearchDialog extends StatefulWidget {
     required this.addSearch,
     required this.removeSearch,
     required this.searches,
+    required this.editSearch,
   }) : super(key: key);
 
   @override
@@ -18,79 +21,132 @@ class CreateSearchDialog extends StatefulWidget {
 
 class _CreateSearchDialogState extends State<CreateSearchDialog> {
   final TextEditingController _searchController = TextEditingController();
-
+  bool isEditing = false;
+  int editingIndex = -1;
   @override
   Widget build(BuildContext context) {
     return StatefulBuilder(builder: (stfContext, stfSetState) {
-      return AlertDialog(
-        title: const Text('Create/Edit Searches'),
-        content: SizedBox(
-            height: 300,
-            width: 300,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  children: [
-                    Expanded(
-                      child: TextField(
-                        controller: _searchController,
-                        decoration:
-                            const InputDecoration(hintText: 'New search'),
-                      ),
+      return DynamicDialog(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Text(
+                      'Create/Edit Searches',
+                      style: Theme.of(context).textTheme.headline6,
                     ),
-                    IconButton(
-                      icon: const Icon(Icons.add),
-                      onPressed: () {
-                        final searchTerm = _searchController.text;
-                        if (searchTerm.isNotEmpty) {
-                          widget.addSearch(searchTerm);
-                          _searchController.clear();
-                          stfSetState(() {});
-                        }
-                      },
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.close),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: _searchController,
+                      decoration: const InputDecoration(hintText: 'New search'),
                     ),
-                  ],
-                ),
-                const SizedBox(height: 16),
-                ListView.builder(
-                  shrinkWrap: true,
-                  itemCount: widget.searches.length,
-                  itemBuilder: (context, index) {
-                    return ListTile(
-                      title: Text(widget.searches[index]),
-                      trailing: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          IconButton(
-                            icon: const Icon(Icons.edit),
-                            onPressed: () {
-                              // Implement edit functionality
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.add),
+                    onPressed: () {
+                      final searchTerm = _searchController.text;
+                      if (searchTerm.isNotEmpty) {
+                        widget.addSearch(searchTerm);
+                        _searchController.clear();
+                        stfSetState(() {});
+                      }
+                    },
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 16),
+            Expanded(
+              child: ListView.builder(
+                shrinkWrap: true,
+                itemCount: widget.searches.length,
+                itemBuilder: (context, index) {
+                  String editedText = widget.searches[index];
+                  return ListTile(
+                    title: isEditing && editingIndex == index
+                        ? TextFormField(
+                            initialValue: widget.searches[index],
+                            onChanged: (value) {
+                              editedText = value;
                             },
-                          ),
-                          IconButton(
-                            icon: const Icon(Icons.delete),
-                            onPressed: () {
-                              widget.removeSearch(index);
-                              stfSetState(() {});
+                            onFieldSubmitted: (value) {
+                              widget.editSearch(index, editedText);
+                              setState(() {
+                                isEditing = false;
+                                editingIndex = -1;
+                              });
                             },
-                          ),
-                        ],
-                      ),
-                    );
-                  },
-                ),
-              ],
-            )),
-        actions: [
-          ElevatedButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            child: const Text('Done'),
-          ),
-        ],
+                          )
+                        : Text(widget.searches[index]),
+                    trailing: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        IconButton(
+                          icon: isEditing && editingIndex == index
+                              ? const Icon(Icons.check)
+                              : const Icon(Icons.edit),
+                          onPressed: () {
+                            setState(() {
+                              if (isEditing && editingIndex == index) {
+                                widget.editSearch(index, editedText);
+                                isEditing = false;
+                                editingIndex = -1;
+                              } else {
+                                isEditing = true;
+                                editingIndex = index;
+                              }
+                            });
+                          },
+                        ),
+                        IconButton(
+                          icon: const Icon(Icons.delete),
+                          onPressed: () {
+                            widget.removeSearch(index);
+                            stfSetState(() {});
+                          },
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.only(top: 16.0, right: 16.0, bottom: 16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Text('Done'),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       );
     });
   }
