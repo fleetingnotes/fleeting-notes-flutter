c69ef0c7312f99214d8235e29bbdca4c1aa23133
Matthew Wong <matthewwong525@gmail.com>
2023-08-20 00:02:01 -0700

    update mobile toolbar (#870)
    * update mobile toolbar
    
    * fix lint
    
    * add overlay and ability to switch toolbarstate
    
    * adjust kb buttons
    
    * add processing buttons
    
    * fix onaddattachment and remove tags from menu
    
    * update icon
    
    * fix bug where toolbar is hidden if cancel image select

--- lib/screens/note/components/ContentField/keyboard_button.dart
+++ lib/screens/note/components/ContentField/keyboard_button.dart
@@ -3,32 +3,35 @@ import 'package:flutter/material.dart';
 class KeyboardButton extends StatelessWidget {
   const KeyboardButton({
     Key? key,
-    required this.icon,
+    required this.child,
     this.onPressed,
     this.tooltip,
     this.disabled = false,
   }) : super(key: key);
 
-  final dynamic icon;
+  final Widget child;
   final VoidCallback? onPressed;
   final String? tooltip;
   final bool disabled;
 
   @override
   Widget build(BuildContext context) {
-    return IconButton(
-      onPressed: (disabled) ? null : () => onPressed?.call(),
-      icon: icon is String
-          ? Text(
-              icon,
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.w700,
-                color: (disabled) ? Colors.grey : null,
-              ),
-            )
-          : Icon(icon, size: 20, color: (disabled) ? Colors.grey : null),
-      tooltip: tooltip,
+    return Container(
+      padding: const EdgeInsets.only(left: 5, top: 3, bottom: 3),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          elevation: 1.0, // Initial elevation
+          backgroundColor: Theme.of(context).colorScheme.background,
+          foregroundColor: Theme.of(context).colorScheme.onBackground,
+          visualDensity: VisualDensity.compact,
+          padding: const EdgeInsets.symmetric(horizontal: 8),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15),
+          ),
+        ),
+        onPressed: onPressed,
+        child: child,
+      ),
     );
   }
 }
