e2c9212de6151478127f17dea780e5967f849b69
Matthew Wong <matthewwong525@gmail.com>
2023-09-13 17:19:30 -0400

    Bump version to 0.11.1

--- windows/build.dart
+++ windows/build.dart
@@ -10,7 +10,7 @@ void main() {
   InnoSetup(
     app: InnoSetupApp(
       name: 'Fleeting Notes',
-      version: Version.parse('0.11.0'),
+      version: Version.parse('0.11.1'),
       publisher: 'Matthew Wong',
       urls: InnoSetupAppUrls(
         homeUrl: Uri.parse('https://fleetingnotes.app/'),
