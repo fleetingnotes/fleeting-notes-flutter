62e1d7087230527755b8f8b0d11d247b43a40f1e
Matthew Wong <matthewwong525@gmail.com>
2023-09-13 17:11:19 -0400

    Revert "Preserve state between opening and closing notes (#907)"
    This reverts commit 27acb960009c6bb57bbf1bb1b20fec99aa23767a.

--- lib/services/settings.dart
+++ lib/services/settings.dart
@@ -25,7 +25,6 @@ class Settings {
     'search-is-list-view': bool,
     'auto-focus-title': bool,
     'right-to-left': bool,
-    'preview': bool,
     // local sync settings
     'local-sync-enabled': bool,
     'local-sync-dir': String,
