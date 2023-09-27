71559a257749658118c1d8cc8668c5735401b887
Marlond <marlondcriollo@gmail.com>
2023-09-27 04:15:25 -0700

    removed excess imports

--- test/textfield_toolbar_test.dart
+++ test/textfield_toolbar_test.dart
@@ -7,23 +7,12 @@
 
 import 'package:fleeting_notes_flutter/my_app.dart';
 import 'package:fleeting_notes_flutter/screens/note/components/ContentField/content_field.dart';
-import 'package:fleeting_notes_flutter/screens/note/components/ContentField/link_preview.dart';
-import 'package:fleeting_notes_flutter/screens/note/components/ContentField/link_suggestions.dart';
 import 'package:fleeting_notes_flutter/screens/note/components/ContentField/textfield_toolbar.dart';
-import 'package:fleeting_notes_flutter/screens/note/components/SourceField/source_container.dart';
-import 'package:fleeting_notes_flutter/screens/note/components/title_field.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:http/http.dart';
-import 'package:http/testing.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:fleeting_notes_flutter/models/Note.dart';
-import 'mocks/mock_supabase.dart';
 import 'utils.dart';
-import 'package:fleeting_notes_flutter/screens/note/components/ContentField/keyboard_button.dart';
-import 'package:fleeting_notes_flutter/screens/note/components/ContentField/textfield_toolbar.dart';
-import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   setUpAll(() {
